a7fdab496ab295e0346ef256e0cd7ea01ce5c358
LizaUrbina <michellurbina77@gmail.com>
2018-09-28 02:29:13 +0200

    xd

---
+++ Joomla/administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-09-28 00:26:24 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-09-28T00:26:24+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-09-28T00:26:31+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-09-28T00:27:14+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
